b844dc16b0850a4572fa7ccd4345b84d4215ce1a
tmanley1985 <tmanley1985@gmail.com>
2017-05-30 11:28:00 -0400

    Added sync method to CustomObjectApi method

---
+++ src/Api/CustomObjectApi.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace GenTux\Marketo\Api;
+
+use GenTux\Marketo\Client;
+use GenTux\Marketo\Exceptions\LeadDoesNotExistException;
+use GenTux\Marketo\Exceptions\MarketoApiException;
+
+class CustomObjectApi extends BaseApi
+{
+    /** @var $customObjectName */
+    protected $customObjectName;
+    /**
+     * CustomObjectApi constructor.
+     * @param string $customObjectName
+     * @param Client $client
+     */
+    public function __construct(Client $client, $customObjectName)
+    {
+        parent::__construct($client);
+        $this->customObjectName = $customObjectName;
+    }
+
+    /**
+     * Create or Update custom object
+     *
+     * @param array $customObject
+     * @param string $lookupField
+     * @param string $action
+     * @throws MarketoApiException
+     */
+    public function sync(
+        array $customObjects,
+        $lookupField = "email",
+        $action = "createOrUpdate"
+    )
+    {
+        $url = $this->client->url . '/rest/v1/customobjects/' . $this->customerObjectName . '.json';
+        $response = $this->post($url , [
+            'action' => $action,
+            'lookupField' => $lookupField,
+            'input' => $customObjects 
+        ]);
+
+        if (!$response->success) {
+            throw new MarketoApiException('Error syncing custom object: ' . $response->errors[0]->message);
+        }
+    }
+}
